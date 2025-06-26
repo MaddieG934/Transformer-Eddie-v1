238cee492d5f5db0b35b1fa9d3d9cb37b78431bc
MaddieG934 <madelynwesthuyzen@gmail.com>
2025-06-26 10:02:48 -0700

    Corrected error in config.ipynb and config.py by importing Path from pathlib

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+
 def get_config():
     return {
         'batch_size': 8,
@@ -18,4 +21,4 @@ def get_weights_file_path(config, epoch: str):
     model_folder = config['model_folder']
     model_basename = config['model_basename']
     model_filename = f"{model_basename}{epoch}.pt"
-    return str(Path('.') / model_folder / model_filename)
+    return str(Path['.'] / model_folder / model_filename)
