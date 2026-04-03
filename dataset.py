23c4a092ae6cccb267d2d6e1a19621ac7476b634
MaddieG934 <madelynwesthuyzen@gmail.com>
2026-04-03 09:15:29 -0700

    Syntax error fix

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
+from collections import Counter
 
 class BilingualDataset(Dataset):
     def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len) -> None:
